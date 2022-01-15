1894a54a8be27e28c7c0f3c7f29396f3f98bde04
Joshua Kraft <nathan.joshua.kraft@gmx.de>
2022-01-15 17:54:17 +0100

    adds all calculations and fetches

--- src/BabydogeDashboard.jsx
+++ src/BabydogeDashboard.jsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from "react";
+import logo from "./images/babydogecoin.png";
+import doge from "./images/babydoge.png";
 
 function BabydogeDashboard() {
-  const [coins, setCoins] = useState(5);
+  const [coinCount, setCoinCount] = useState(400000000000);
+  const [coinValue, setCoinValue] = useState(0.000002);
+
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 
   useEffect(() => {
-    async function fetchCoinData() {
+    async function fetchCoinCount() {
       try {
         let url =
           "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xc748673057861a797275CD8A068AbB95A902e8de&address=0x9c41f4215152d3c431f29c228afb0cf4b5a1a2d8&tag=latest&apikey=";
@@ -13,21 +20,54 @@ function BabydogeDashboard() {
         console.log("Fetching API succeded");
         const data = await response.json();
         console.log(data);
-        const coinCount = (data.result * 1) / 1000000000;
-        setCoins(coinCount);
+        const coinAmount = (data.result * 1) / 1000000000;
+        setCoinCount(coinAmount);
       } catch (err) {
         console.log("Fetching API endpoint failed");
         console.log(err);
       }
     }
 
-    fetchCoinData();
+    async function fetchCoinValue() {
+      try {
+        let url =
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=baby-doge-coin";
+        const response = await fetch(url);
+        console.log("Fetching API succeded");
+        const data = await response.json();
+        console.log(data[0]);
+        const coinPrice = data[0].current_price;
+        setCoinValue(coinPrice);
+      } catch (err) {
+        console.log("Fetching API endpoint failed");
+        console.log(err);
+      }
+    }
+
+    fetchCoinCount();
+    fetchCoinValue();
   }, []);
 
   return (
-    <div>
-      <h1>Babydogecoin</h1>
-      <h3>{coins}</h3>
+    <div className="content">
+      <nav className="nav">
+        <img className="nav-logo" src={logo} />
+        <h1 className="nav-title">Babydogecoin</h1>
+      </nav>
+      <div className="top-section">
+        <h1 className="count-display">
+          {numberWithCommas(Math.floor(coinCount))}
+          <img className="count-coin" src={logo} />
+        </h1>
+      </div>
+      <div className="value-container">
+        <h3 className="value-display">
+          {numberWithCommas(Math.floor(coinCount * coinValue))} $
+        </h3>
+      </div>
+      <div className="babydoge-container">
+        <img className="babydoge" src={doge} />
+      </div>
     </div>
   );
 }
