f33f3e48997082a294fed6c67ce9cdc1bdcd0fb2
Joshua Kraft <nathan.joshua.kraft@gmx.de>
2022-01-16 10:41:51 +0100

    fixes and advances periodically fetches

--- src/BabydogeDashboard.jsx
+++ src/BabydogeDashboard.jsx
@@ -3,52 +3,64 @@ import logo from "./images/babydogecoin.png";
 import doge from "./images/babydoge.png";
 
 function BabydogeDashboard() {
-  const [coinCount, setCoinCount] = useState(99999999999999);
-  const [coinValue, setCoinValue] = useState(0.000000002);
+  const [coinCount, setCoinCount] = useState(0);
+  const [coinValue, setCoinValue] = useState(2);
+
+  let ADRESS = "0x9c41f4215152d3c431f29c228afb0cf4b5a1a2d8";
+  let CONTRACT_ADRESS = "0xc748673057861a797275CD8A068AbB95A902e8de";
+  let MINUTE_INTERVAL = 8;
 
   function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
+  async function fetchCoinCount() {
+    try {
+      let url = `https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${CONTRACT_ADRESS}&address=${ADRESS}&tag=latest&apikey=`;
+      url += process.env.REACT_APP_BSCSCAN_API_KEY;
+      const response = await fetch(url);
+      console.log("Fetching API succeded");
+      const data = await response.json();
+      console.log(data);
+      const coinAmount = (data.result * 1) / 1000000000;
+      setCoinCount(coinAmount);
+    } catch (err) {
+      console.log("Fetching API endpoint failed");
+      console.log(err);
+    }
+  }
+
+  async function fetchCoinValue() {
+    try {
+      let url =
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=baby-doge-coin";
+      const response = await fetch(url);
+      console.log("Fetching API succeded");
+      const data = await response.json();
+      console.log(data[0]);
+      const coinPrice = data[0].current_price;
+      setCoinValue(coinPrice);
+    } catch (err) {
+      console.log("Fetching API endpoint failed");
+      console.log(err);
+    }
+  }
+
   useEffect(() => {
-    let ADRESS = "0x9c41f4215152d3c431f29c228afb0cf4b5a1a2d8";
-    let CONTRACT_ADRESS = "0xc748673057861a797275CD8A068AbB95A902e8de";
-    let MINUTE_INTERVAL = 8;
+    fetchCoinCount();
+    fetchCoinValue();
 
-    async function fetchCoinCount() {
-      try {
-        let url = `https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${CONTRACT_ADRESS}&address=${ADRESS}&tag=latest&apikey=`;
-        url += process.env.REACT_APP_BSCSCAN_API_KEY;
-        const response = await fetch(url);
-        console.log("Fetching API succeded");
-        const data = await response.json();
-        console.log(data);
-        const coinAmount = (data.result * 1) / 1000000000;
-        setCoinCount(coinAmount);
-      } catch (err) {
-        console.log("Fetching API endpoint failed");
-        console.log(err);
-      }
-    }
+    const intervalCount = setInterval(() => {
+      fetchCoinCount();
+    }, MINUTE_INTERVAL * 60000);
+    const intervalValue = setInterval(() => {
+      fetchCoinValue();
+    }, MINUTE_INTERVAL * 60000);
 
-    async function fetchCoinValue() {
-      try {
-        let url =
-          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=baby-doge-coin";
-        const response = await fetch(url);
-        console.log("Fetching API succeded");
-        const data = await response.json();
-        console.log(data[0]);
-        const coinPrice = data[0].current_price;
-        setCoinValue(coinPrice);
-      } catch (err) {
-        console.log("Fetching API endpoint failed");
-        console.log(err);
-      }
-    }
-
-    setInterval(fetchCoinCount(), MINUTE_INTERVAL * 60000);
-    setInterval(fetchCoinValue(), MINUTE_INTERVAL * 60000);
+    return () => {
+      clearInterval(intervalCount);
+      clearInterval(intervalValue);
+    };
   }, []);
 
   return (
